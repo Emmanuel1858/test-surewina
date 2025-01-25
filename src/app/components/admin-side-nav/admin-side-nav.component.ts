6b9e20aa1e5ff4371f6013d500c813af4067d072
Babaneh <travisneh1996@gmail.com>
2025-01-25 07:18:39 +0100

    added the router-link = active feature

--- src/app/components/admin-side-nav/admin-side-nav.component.ts
+++ src/app/components/admin-side-nav/admin-side-nav.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-admin-side-nav',
   templateUrl: './admin-side-nav.component.html',
@@ -7,9 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AdminSideNavComponent {
    
-  constructor() {
+  constructor(private router: Router) {
 
   }
+
+  
+  // navigateToDashboard() {
+  //   this.router.navigate(['/admin-dashboard'])
+  // }
+
+  // navigateToUser() {
+  //   this.router.navigate(['/users'])
+  // }
+
+  // navigateToLottery() {
+  //   this.router.navigate(['/lottery'])
+  // }
+
+  // navigateToTrans() {
+  //   this.router.navigate(['/transaction'])
+  //   console.log('ugiyft')
+  // }
 
   userName: string = 'Dasola A';
   userEmail: string = 'dasola_dzn@gmail.com';
