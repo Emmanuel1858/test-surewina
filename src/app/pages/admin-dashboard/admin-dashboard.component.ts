5ddc1fabccb731413e5752187156aa26b9f79734
Babaneh <travisneh1996@gmail.com>
2025-01-26 23:48:07 +0100

    worked on the cta-cards in card-4

--- src/app/pages/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/admin-dashboard/admin-dashboard.component.ts
@@ -8,12 +8,36 @@ import { Component } from '@angular/core';
 export class AdminDashboardComponent {
 
   tabs: string = 'prizes'; 
+  ctaPosition: number = 1;
+  h2ctaText: string = 'Setup a draw';
+  pctaText: string = 'Schedule monthly or weekly draws';
+  imgSrc: string = '../.././../assets/configuration-02.svg';
 
   ngOnInit() {  }
 
 
   switchTab(tab: string) {
     this.tabs = tab;
+  }
+
+  // this cannot work cause designer did not export the necessary files
+  // need to export items in a particular way
+
+  ctaFlip() {
+    switch(this.ctaPosition) {
+      case 2:
+        this.imgSrc = '../.././../assets/configuration-02.svg'
+        this.h2ctaText = 'Create a ticket';
+        this.pctaText = 'Configure ticket details and prices';
+        break;
+      case 3: 
+        this.h2ctaText = 'Add new prize';
+        this.pctaText = 'Add a new prize into the system';
+        break;
+      default: 
+        this.h2ctaText = 'Setup a draw';
+        this.pctaText = 'Schedule monthly or weekly draws';
+    }
   }
 
 
